90532710e60dbbd3d652a923f3a2338927d8157f
Nikolaos Tselikas - Kourtidis <nikolaos.wolf@hotmail.com>
2019-04-15 11:51:18 +0300

    Removing EntityFrameworkCore

--- SmartCoziness/SmartCoziness/Models/CozySmartContext.cs
+++ SmartCoziness/SmartCoziness/Models/CozySmartContext.cs
@@ -9,7 +9,7 @@ namespace SmartCoziness.Models
 {
     public class CozySmartContext : DbContext
     {
-        const string connectionString = @"Data Source=LAPTOP-6K7LN3AO\SQLEXPRESS2017;Initial Catalog=CozySmart;Integrated Security=True";
+        const string connectionString = @"Data Source=DESKTOP-CT329F2\SQLEXPRESS01;Initial Catalog=CozySmart;Integrated Security=True";
 
         public CozySmartContext() : base(connectionString) { }
 
@@ -28,10 +28,13 @@ namespace SmartCoziness.Models
         /// </summary>
         public DbSet<Accommodation> Accommodations { get; set; }
 
+        /// <summary>
+        /// Collection managing the bookings of the tenants/accommodations
+        /// </summary>
         public DbSet<Booking> Bookings { get; set; }
 
         /// <summary>
-        /// Collection managing the images of the properties
+        /// Collection managing the images of the accommodations
         /// </summary>
         public DbSet<Image> Images { get; set; }
 
