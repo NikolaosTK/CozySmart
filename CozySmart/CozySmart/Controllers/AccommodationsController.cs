c4ec7d9eaad42020e5ec0f56cd0f78557aa9bf9e
Nikolaos Tselikas - Kourtidis <nikolaos.wolf@hotmail.com>
2019-04-26 14:51:30 +0300

    Implemented the Identity Framework and reworked the entire project.

---
+++ CozySmart/CozySmart/Controllers/AccommodationsController.cs
@@ -0,0 +1,22 @@
+﻿using CozySmart.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CozySmart.Controllers
+{
+    public class AccommodationsController : Controller
+    {
+        // GET: Accommodations
+        public ActionResult Index()
+        {
+
+
+            return View();
+        }
+
+    }
+}
