9173b962ba36ed7e665d3eafbc96a8320800b72b
Nikolakopoulos <basilis.nikolakopoulos@gmail.com>
2019-05-11 23:31:17 +0300

    Add roles (Tenant , Host)

---
+++ CozySmart/CozySmart/Controllers/RolesController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using CozySmart.Models;
+using CozySmart.ViewModels;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace CozySmart.Controllers
+{
+    public class RolesController : Controller
+    {
+
+
+        private CozySmartContext _db;
+
+        public RolesController()
+        {
+            _db = new CozySmartContext();
+        }
+        // GET: Roles
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult AddHostRole()
+        {
+
+
+
+            var userStore = new UserStore<ApplicationUser>(_db);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+
+
+            var s = userManager.GetRoles(User.Identity.GetUserId());
+
+            foreach (var role in s)
+            {
+                if (role.ToString() == "Host")
+                {
+                    ViewBag.message = " You have already been a host ";
+                    break;
+                }
+
+                else
+                {
+
+                    userManager.AddToRole(User.Identity.GetUserId(), "Host");
+
+                    ViewBag.message = "You are also Host";
+                }
+
+               
+            }  
+             
+
+            return View();
+
+        }
+
+
+    }
+}
